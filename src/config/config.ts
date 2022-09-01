1d7c3a7ba986a8ea28ebc03efcd42b8309e7630d
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-09-01 12:46:37 +0300

    patch: aws profile name update

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ export const config = {
     "host": "udagramrotichdev.c3asfjs4ajj2.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-1",
-    "aws_profile": "udacity-kev-dev1",
+    "aws_profile": "udagram-kev-dev1",
     "aws_media_bucket": "udagram-rotich-dev"
   },
   "jwt": {
