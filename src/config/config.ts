cd484195e12947c0b8753df410d3e56416a9883c
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-09-01 12:33:45 +0300

    ft: sequalize & configurations for RDS

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "udagramrotichdev",
+    "password": "SEMBERUA",
+    "database": "udagramrotichdev",
+    "host": "udagramrotichdev.c3asfjs4ajj2.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "udacity-kev-dev1",
+    "aws_media_bucket": "udagram-rotich-dev"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
